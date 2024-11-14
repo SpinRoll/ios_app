7e9e89e222b40578628ab3c7e24b4478d43c1445
marco-spino <37246105-marco-spino@users.noreply.replit.com>
2024-11-14 16:10:08 +0000

    Restored to '9aa9acdb941cdacda617c0ab2f5de830727c0cba'
    Replit-Restored-To:9aa9acdb941cdacda617c0ab2f5de830727c0cba

--- replit.nix
+++ replit.nix
@@ -1,17 +1,5 @@
 {pkgs}: {
   deps = [
-    pkgs.cairo
-    pkgs.zlib
-    pkgs.tk
-    pkgs.tcl
-    pkgs.openjpeg
-    pkgs.libxcrypt
-    pkgs.libwebp
-    pkgs.libtiff
-    pkgs.libjpeg
-    pkgs.libimagequant
-    pkgs.lcms2
-    pkgs.freetype
     pkgs.postgresql
     pkgs.openssl
   ];
